462c6c25389e8917f0fce3139caa3c62416bbf7b
Dennisemm <23-02812@g.batstate-u.edu.ph>
2024-11-28 13:12:51 +0800

    Add files via upload

---
+++ Laboratory Activity 2 - (28) - (11) - (2024)/MultiplicationTable.java.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the maximum number for the multiplication table: ");
+        int maxNumber = scanner.nextInt();
+
+        int[][] multiplicationTable = new int[maxNumber + 1][maxNumber + 1];
+
+        for (int i = 0; i <= maxNumber; i++) {
+            for (int j = 0; j <= maxNumber; j++) {
+                multiplicationTable[i][j] = i * j;
+            }
+        }
+
+        for (int i = 0; i <= maxNumber; i++) {
+            for (int j = 0; j <= maxNumber; j++) {
+                System.out.printf("%4d", multiplicationTable[i][j]);
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
